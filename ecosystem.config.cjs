d9594e0222ab16dcdb8216ca82a7b092d6d932ba
Tadeuchi <tadeuszwl@gmail.com>
2025-01-23 15:31:01 +0100

    ao - ecosystem config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    apps: [
+        {
+            name: "agent-smith",
+            script: "pnpm",
+            node_args: "-r dotenv/config",
+            args: "start --character='characters/agent-smith.character.json'  dotenv_config_path=./smith.env",
+            env: {
+                PORT: "3000",
+            },
+        },
+        {
+            name: "agent-neo",
+            script: "pnpm",
+            node_args: "-r dotenv/config",
+            args: "start --character='characters/neo.character.json' dotenv_config_path=./neo.env",
+            env: {
+                PORT: "3001",
+            },
+        },
+        {
+            name: "agent-morpheus",
+            script: "pnpm",
+            node_args: "-r dotenv/config",
+            args: "start --character='characters/morpheus.character.json' dotenv_config_path=./morpheus.env",
+            env: {
+                PORT: "3002",
+            },
+        },
+    ],
+};
