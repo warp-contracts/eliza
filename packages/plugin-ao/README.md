70319b859fc4bd35200387581bcbc35ebfaf8b4d
Asia <ziola.jm@gmail.com>
2025-01-24 19:24:57 +0100

    readme update

--- packages/plugin-ao/README.md
+++ packages/plugin-ao/README.md
@@ -5,9 +5,15 @@ A plugin for interacting with the C.L.A.R.A. protocol on [AO network](https://ao
 ## Overview
 
 This plugin provides functionality to delegate tasks to the AI agents registered on marketplace using C.L.A.R.A. protocol.
-
+`@elizaos/plugin-ao` is currently available in the [following Eliza fork](https://github.com/redstone-finance/eliza) (branch - `twl/paris`)
 
 ## Configuration
+
+`.env` file is required, in order to generate a new one for the project copy existing example
+
+```bash
+cp .env.example .env
+```
 
 The plugin requires the following environment variables:
 
@@ -19,15 +25,14 @@ AO_WALLET_ID=    # Arweave wallet address
 ```
 
 At the moment we are using a testing marketplace id: `ynXmUtQUgi3eAGF6TjNxS6Wo0uu228E7fGlmIzocc7U`
-All the Env variables required for AO plugin can be generated using the following script:
+All the Env variables required for AO plugin can be generated using the following script (run this script from the root of your project)
 
 ```bash
 node ./scripts/setupClaraProfile.mjs
 ```
 
-At the start the agent profile is automatically generated and profile id is stored in dir `./profiles`.
+When delegating first task the agent profile is automatically generated and profile id is stored in dir `./profiles`.
 The profile is also automatically registered on marketplace.
-
 
 ## Installtion
 
@@ -35,32 +40,29 @@ The profile is also automatically registered on marketplace.
 pnpm install --no-frozen-lockfile
 ```
 
-
 ## Building
 
 ```bash
 pnpm build
 ```
 
-
-
-
 ## Usage
 
-In order to delegate tasks to AI agents: 
- - Start the agent 
+In order to delegate tasks to AI agents:
 
-```bash 
-pnpm start 
+- Start the agent
+
+```bash
+pnpm start
 ```
 
- -  Start the web client
+- Start the web client
 
 ```bash
 pnpm start:client
 ```
 
- - Go to [http://localhost:5173/](http://localhost:5173/) and start communication using the following format
+- Go to [http://localhost:5173/](http://localhost:5173/) and start communication using the following format
 
 ```
 I have a task for you: using clara protocol post tweet about immune system
--- packages/plugin-ao/src/actions/task.ts
+++ packages/plugin-ao/src/actions/task.ts
@@ -11,9 +11,8 @@ import {
     type Action,
 } from "@elizaos/core";
 import { z } from "zod";
-import {claraProfileProvider} from "../clara/ClaraProfileProvider.ts";
+import { claraProfileProvider } from "../clara/ClaraProfileProvider.ts";
 import { TOPICS, MATCHERS } from "redstone-clara-sdk";
-
 
 const claraTaskTemplate = `
 Using request message respond with a request from the initial message, return a block containing only the extracted task.
@@ -58,9 +57,7 @@ Respond with a JSON markdown block containing only the extracted values. Use nul
 
 export const task: Action = {
     name: "CLARA_TASK",
-    similes: [
-        "TASKS", "CLARA_TASK", "REQUEST", "C.L.A.R.A"
-    ],
+    similes: ["TASKS", "CLARA_TASK", "REQUEST", "C.L.A.R.A"],
     suppressInitialMessage: true,
     validate: async (runtime: IAgentRuntime, message: Memory) => {
         elizaLogger.debug("Validating :", message.userId);
@@ -72,7 +69,8 @@ export const task: Action = {
         }
         return true;
     },
-    description: "C.L.A.R.A, Use CLARA_TASK always if there is any mention of task, request, order, clara task or clara protocol.",
+    description:
+        "C.L.A.R.A, Use CLARA_TASK always if there is any mention of task, request, order, clara task or clara protocol.",
     handler: async (
         runtime: IAgentRuntime,
         message: Memory,
@@ -82,22 +80,26 @@ export const task: Action = {
     ): Promise<boolean> => {
         elizaLogger.log("Starting CLARA_TASK handler...");
 
-        const claraProfile = await claraProfileProvider.get(runtime, message, state);
+        const claraProfile = await claraProfileProvider.get(
+            runtime,
+            message,
+            state
+        );
 
         // // Define the schema for the expected output
         const taskSchema = z.object({
             action: z.string(),
             payload: z.string(),
             strategy: z.string(),
-            action_count: z.number()
+            action_count: z.number(),
         });
 
-        state.actions = TOPICS.join(', ')
-        state.strategies = MATCHERS.join(', ')
-        elizaLogger.debug("Actions", state.actions)
-        elizaLogger.debug("Strategies", state.strategies)
-        state.task = message.content.text
-        elizaLogger.debug("Task command", state.task)
+        state.actions = TOPICS.join(", ");
+        state.strategies = MATCHERS.join(", ");
+        elizaLogger.debug("Actions", state.actions);
+        elizaLogger.debug("Strategies", state.strategies);
+        state.task = message.content.text;
+        elizaLogger.debug("Task command", state.task);
         // Compose transfer context
         const context = composeContext({
             state,
@@ -114,16 +116,19 @@ export const task: Action = {
                 modelClass: ModelClass.SMALL,
             });
         } catch (e) {
-            elizaLogger.error(`AO plugin: failed to generate task based on message`, message.content, e);
+            elizaLogger.error(
+                `AO plugin: failed to generate task based on message`,
+                message.content,
+                e
+            );
             if (callback) {
                 callback({
                     text: `Cannot generate CLARA task based on input, try again`,
-                    content: '',
+                    content: "",
                 });
             }
             return false;
         }
-
 
         // Sending request using CLARA SDK
         const responses = {};
@@ -140,39 +145,55 @@ export const task: Action = {
                     topic: taskObject.action,
                     reward: 10,
                     matchingStrategy: taskObject.strategy,
-                    payload: taskObject.payload
+                    payload: taskObject.payload,
                 });
                 elizaLogger.debug(`${i} task registered`, result);
                 responses[result.taskId] = formatTaskAssigment(i, result);
             }
         } catch (e) {
-            elizaLogger.error(`AO plugin: failed to send request using CLARA SDK`, message.content, e);
+            elizaLogger.error(
+                `AO plugin: failed to send request using CLARA SDK`,
+                message.content,
+                e
+            );
             if (callback) {
                 callback({
                     text: `Cannot send CLARA request`,
-                    content: '',
+                    content: "",
                 });
             }
             return false;
         }
 
-
         try {
-            await pollTaskResult(claraProfile, Object.keys(responses)).then((res) => {
-                elizaLogger.debug(`updating message`, res);
-                for (const [key, value] of Object.entries(res)) {
-                    responses[key] = responses[key] + formatTwitterMessage(value);
+            await pollTaskResult(claraProfile, Object.keys(responses)).then(
+                (res) => {
+                    elizaLogger.debug(`updating message`, res);
+                    for (const [key, value] of Object.entries(res)) {
+                        responses[key] =
+                            responses[key] + formatTwitterMessage(value);
+                    }
                 }
-            });
+            );
             if (callback) {
-                callback({ text: Object.values(responses).join('\n\n-----------------------\n\n') });
+                callback({
+                    text: Object.values(responses).join(
+                        "\n\n-----------------------\n\n"
+                    ),
+                });
             }
             return true;
         } catch (e) {
-            elizaLogger.error(`AO plugin: failed to fetch request result using CLARA SDK`, message.content, e);
+            elizaLogger.error(
+                `AO plugin: failed to fetch request result using CLARA SDK`,
+                message.content,
+                e
+            );
             if (callback) {
                 callback({
-                    text: Object.values(responses).join('\n\n-----------------------\n\n')
+                    text: Object.values(responses).join(
+                        "\n\n-----------------------\n\n"
+                    ),
                 });
             }
         }
@@ -228,22 +249,26 @@ export const task: Action = {
                     strategy: "cheapest",
                     action_count: 3,
                 },
-            }
+            },
         ],
     ] as ActionExample[][],
 } as Action;
 
-
 function formatTaskAssigment(i: number, result): string {
     return `-- Task ${i} \nAssigned to ${result?.assignedAgentId}\nFee: ${result?.fee}
-        \nC.L.A.R.A. req: https://www.ao.link/#/message/${result?.taskId}\n`
+        \nC.L.A.R.A. req: https://www.ao.link/#/message/${result?.taskId}\n`;
 }
 
 function formatTwitterMessage(res): string {
-    return `\nX: https://x.com/${res?.result?.userName}/status/${res?.result?.id}\n`
+    return `\nX: https://x.com/${res?.result?.userName}/status/${res?.result?.id}\n`;
 }
 
-async function pollTaskResult(claraProfile, reqTaskId: Array<string>, interval = 10, duration = 180) {
+async function pollTaskResult(
+    claraProfile,
+    reqTaskId: Array<string>,
+    interval = 10,
+    duration = 300
+) {
     const startTime = Date.now();
     let total = reqTaskId.length;
     const results = {};
@@ -252,7 +277,7 @@ async function pollTaskResult(claraProfile, reqTaskId: Array<string>, interval =
         const intervalId = setInterval(async () => {
             const elapsedTime = Date.now() - startTime;
 
-            if (elapsedTime >= (duration * 1000)) {
+            if (elapsedTime >= duration * 1000) {
                 clearInterval(intervalId);
                 resolve(results);
                 return;
@@ -261,7 +286,7 @@ async function pollTaskResult(claraProfile, reqTaskId: Array<string>, interval =
             try {
                 const taskResult = await claraProfile.loadNextTaskResult();
                 elizaLogger.debug(`next task result`, taskResult);
-                if (taskResult &&  reqTaskId.includes(taskResult.id)) {
+                if (taskResult && reqTaskId.includes(taskResult.id)) {
                     total -= 1;
                     results[taskResult.id] = taskResult;
                     if (total <= 0) {
@@ -276,5 +301,3 @@ async function pollTaskResult(claraProfile, reqTaskId: Array<string>, interval =
         }, interval * 1000);
     });
 }
-
-
