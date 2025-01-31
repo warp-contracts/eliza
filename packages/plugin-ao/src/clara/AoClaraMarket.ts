0affb769556a8b8db82e80f90f116dd4882a5f29
Asia <ziola.jm@gmail.com>
2025-01-31 12:18:33 +0100

    update clara-sdk to 0.0.16

--- packages/plugin-ao/src/clara/AoClaraMarket.ts
+++ packages/plugin-ao/src/clara/AoClaraMarket.ts
@@ -34,7 +34,7 @@ export class AoClaraMarket {
                     {
                         metadata: { description: this.profileId },
                         topic: "chat",
-                        fee: 999999,
+                        fee: 100000000000,
                         agentId: this.profileId,
                     }
                 );
